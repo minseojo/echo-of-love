633c2f90f8581edac682a323564d3e0d05878372
minseo Jo <liging12@naver.com>
2025-01-18 21:23:13 +0900

    Init: 초기 설정

---
+++ EchoOfLove/src/main/java/EchoOfLove/EchoOfLoveApplication.java
@@ -0,0 +1,13 @@
+package EchoOfLove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoOfLoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoOfLoveApplication.class, args);
+	}
+
+}
---
+++ EchoOfLove/src/test/java/EchoOfLove/EchoOfLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package EchoOfLove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoOfLoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
